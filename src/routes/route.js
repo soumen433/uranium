e7870f64d6238ad1a3d2270e421543c0c007039f
soumen <soumenmaity433@.com>
2022-04-08 10:24:40 +0530

    assginment done

--- src/routes/route.js
+++ src/routes/route.js
@@ -2,17 +2,43 @@ const express = require('express');
 const logger = require('./logger')
 
 const router = express.Router();
+//problem-1
+router.get('/movies', function (req, res) {
+    let moviesList=["rang de basanti","KGF","PUSHPA","THE shining","lord of the ring","Don",]
+    let a=[]
 
-router.get('/test-me', function (req, res) {
-    console.log('------------------')
-    console.log(req)
-    console.log('------------------')
-    console.log('These are the request query parameters: ', req.query)
-    res.send('My first ever api!')
+    for(i=0;i<moviesList.length;i++){
+        a+=moviesList[i]+','
+    }
+    res.send(a)
+});
+//problem-2 && problem-3
+router.get('/movies/:number', function (req, res) {
+    let moviesList=["rang de basanti","KGF","PUSHPA","THE shining","lord of the ring","Don"]
+    let i=req.params.number
+     if (i<=moviesList.length-1){
+res.send(moviesList[i])}
+    else{
+    res.send('use a valid index') 
+    }
 });
 
+//problem-4
+router.get('/films', function (req, res) {
+    let moviesList=[{"id":1,"name":"the shining"},{"id":2,"name":"incendies"},{"id":3,"name":"rang de basanti"},{"id":4,"name":"finding nemo"}]
 
-
-
+    res.send(moviesList)
+});
+//problem -5
+router.get('/films/:filmid', function (req, res) {
+    let moviesList=[{"id":1,"name":"the shining"},{"id":2,"name":"incendies"},{"id":3,"name":"rang de basanti"},{"id":4,"name":"finding nemo"}]
+    let a=req.params.filmid-1
+if(a<=moviesList.length-1){
+    res.send(moviesList[a])
+  }
+else{
+    res.send('no movie exixt with this id')
+    } 
+});
 module.exports = router;
 // adding this comment for no reason
