c1456b0ee952b9b4a94348bf150a8e36021bd077
soumen <soumenmaity433@.com>
2022-04-13 21:42:12 +0530

    done

--- src/models/bookModel.js
+++ src/models/bookModel.js
@@ -1,31 +1,23 @@
 const mongoose = require('mongoose');
 
 const bookSchema = new mongoose.Schema( {
-    bookName: String, 
+    bookName:{ type: String, required:true},
+        
     authorName: String, 
     tags: [String],
+    year:{ type:Number, default: 2021},
+
     
-    isPublished: Boolean,
+    stockAvailable: Boolean,
     prices: {
         indianPrice: String,
         europePrice: String,
     },
-    sales: {type: Number, default: 10}
+    totalPages:Number,
 }, { timestamps: true });
 
 
-module.exports = mongoose.model('Book', bookSchema) //users
+module.exports = mongoose.model('Book2', bookSchema) //users
 
-//Validation:
-//require:true
-//unique
-// default
 
-//String
-//Number
-//Date
-//Boolean
-// Arrays
-// Object
-// ObjectId
-// Buffer - not cover
+
--- src/routes/route.js
+++ src/routes/route.js
@@ -1,19 +1,18 @@
 const express = require('express');
 const router = express.Router();
-// const UserModel= require("../models/userModel.js")
-const UserController= require("../controllers/userController")
+
 const BookController= require("../controllers/bookController")
-
-router.get("/test-me", function (req, res) {
-    res.send("My first ever api!")
-})
-
-router.post("/createUser", UserController.createUser  )
-
-router.get("/getUsersData", UserController.getUsersData)
 
 router.post("/createBook", BookController.createBook  )
 
-router.get("/getBooksData", BookController.getBooksData)
+router.get("/Allbooklist",BookController.Booklist)
+
+router.post("/getbookinYear",BookController.getBooksInyear)
+
+router.post("/perticulerbook",BookController.particularBook)
+
+router.get("/INRbook",BookController.getXINRbooks)
+
+router.get("/getrandomBook",BookController.getRandomBooks)
 
 module.exports = router;
