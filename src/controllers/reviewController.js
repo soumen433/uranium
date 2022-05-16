69bbea4e1a979546d5626595e184c0e6e0605b0a
soumen <soumenmaity433@.com>
2022-05-16 09:55:11 +0530

    done

--- src/controllers/reviewController.js
+++ src/controllers/reviewController.js
@@ -32,10 +32,11 @@ const createReview = async function (req, res) {
         if (!data.rating) {
             return res.status(400).send({ status: false, message: "rating is required" })
         }
+       
         //validating the rating (min:1 and max:5) by using regex
         let isValidRating = /^[1-5]{1}$/
         if (!(isValidRating.test(data.rating))) {
-            return res.status(400).send({ status: false, message: "rating should be from 1 to 5" })
+            return res.status(400).send({ status: false, message: "rating should be from  integer 1 to 5" })
         }
         //updating the reviews key and incrementing its count whenever the new document of a review of that bookId is created
         let updateReviewsOfBook = await bookModel.findOneAndUpdate({ _id: bookId },
@@ -89,7 +90,10 @@ const updateReview = async function (req, res) {
         }
 
         let data = req.body
-
+        let isValidRating = /^[1-5]{1}$/
+        if (!(isValidRating.test(data.rating))) {
+            return res.status(400).send({ status: false, message: "rating should be from  integer 1 to 5" })
+        }
         //checking If any data is given inside body or not
         if (Object.keys(data).length == 0) {
             return res.status(400).send({ status: false, message: "Data must be given for updation" })
