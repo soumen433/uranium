cde0c44b4f9ac1a410e23362295894ea65e52630
soumen <soumenmaity433@.com>
2022-05-23 17:32:12 +0530

    done

--- src/controllers/bookController.js
+++ src/controllers/bookController.js
@@ -115,8 +115,8 @@ const createBook = async function (req, res) {
         if(files && files.length>0){
             //upload to s3 and get the uploaded link
             // res.send the link back to frontend/postman
-            let uploadedFileURL= await uploadFile( files[0] )
-            res.status(201).send({msg: "file uploaded succesfully", data: uploadedFileURL})
+            var uploadedFileURL= await uploadFile( files[0] )
+           // res.status(201).send({msg: "file uploaded succesfully", data: uploadedFileURL})
         }
         else{
             res.status(400).send({ msg: "No file found" })
